aadf49bbc976b1072b6d71db89474a063328bb81
Talha <admin@talha.com>
2024-01-11 11:11:53 +0500

    page not found

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React from "react";
 import "./App.css";
-import { Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Routers from "./Routers";
 
 function App() {
   return (
-    <>
-      <div className="bg-secondary py-3">
-        <header className="container d-flex judtify-content-between align-items-center">
-          <a href="#"></a>
-          <nav>
-            <ul className="list-unstyled d-flex align-items-center mb-0 ">
-              <li className="me-3">
-                <Link to="/">Home</Link>
-              </li>
-              <li className="me-3">
-                <Link to="about">About</Link>
-              </li>
-              <li className="me-3">
-                <Link to="contact">Contact</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-      </div>
-      <Routers />
-    </>
+    <BrowserRouter>
+    <Routers/>   
+    </BrowserRouter>
   );
 }
 export default App;
--- src/Routers.jsx
+++ src/Routers.jsx
@@ -1,15 +1,36 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import About from "./components/about";
 import Contact from "./components/contact";
 import Home from "./components/home";
+import PageNotFound from "./components/pageNotFound";
 
 export default function Routers() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="contact" element={<Contact />} />
-    </Routes>
+    <>
+      <div className="bg-secondary py-3">
+        <header className="container d-flex judtify-content-between align-items-center">
+          <nav>
+            <ul className="list-unstyled d-flex align-items-center mb-0 ">
+              <li className="me-3">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="me-3">
+                <Link to="about">About</Link>
+              </li>
+              <li className="me-3">
+                <Link to="contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} /> 
+        <Route path="*" element={<PageNotFound/>} />      
+      </Routes>
+    </>
   );
 }
